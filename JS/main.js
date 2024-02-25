c3654ba3573312b45b2dd8e2dbd4bf5bdb1f7124
Anderson2il4 <silvaanderson124@gmail.com>
2024-02-25 15:17:29 -0300

    main.js update, singup page update and README update

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,21 @@
-var userNameEmail = document.getElementById('userNameEmail')
-var password = document.getElementById('password')
-
-function alerta() {
-    alert(`Userna/ Email: ${userNameEmail.value}`)
-    alert(`Password: ${password.value}`)
+class User {
+    constructor(userName, email, age, password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
 }
-
 
 function userAdd() {
-    let userName = document.getElementById('userName')
-    let email = document.getElementById('email')
-    let age = document.getElementById('age')
-    let password = document.getElementById('password')
+    let userName = document.getElementById('userName').value
+    let email = document.getElementById('email').value
+    let age = document.getElementById('age').value
+    let password = document.getElementById('password').value
     
     let user = new User(userName, email, age, password)
-    user.print();
 
-    class User {
-        constructor(userName, email, age, password) {
-            this.userName = userName;
-            this.email = email;
-            this.password = password;
-            this.age = age;
-        }
-    }
-
-       
-
-    function print() {
-        console.log(`Name: ${this.userName}`)
-        console.log(`Email: ${this.email}`)
-        console.log(`Passsword: ${this.password}`)
-        console.log(`Age: ${this.age}`)
-    }
+    console.log(user)
+    debugger;
 }
+
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ A nice way to send messages
 
 ## Used features
 
-1 - HTML, CSS and Java Script  
+1 - HTML, CSS, jQuery, PHP and Java Script //You'll need to configure a PHP server   
 2 - User authentication  
 3 - Cryptography
